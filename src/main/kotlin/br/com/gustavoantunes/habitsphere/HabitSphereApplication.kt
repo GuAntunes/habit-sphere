afcdc5ef9cc7fedff256260a4f9a0d06fb200a76
Gustavo Antunes <gustavo.silva@ztech.net>
2023-09-15 20:27:33 -0300

    First commit

---
+++ src/main/kotlin/br/com/gustavoantunes/habitsphere/HabitSphereApplication.kt
@@ -0,0 +1,11 @@
+package br.com.gustavoantunes.habitsphere
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HabitSphereApplication
+
+fun main(args: Array<String>) {
+	runApplication<HabitSphereApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/gustavoantunes/habitsphere/HabitSphereApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.gustavoantunes.habitsphere
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HabitSphereApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
